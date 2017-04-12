00bfb56d40becfd90a81488421aa748411d1f0d3
Pavel Stetina <github@shark.kom.cz>
2017-04-12 23:59:21 +0200

    Create /etc/timezone and /var/log/www only if missing

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
-cp /vagrant/vendor/sharkodlak/development/filesystem/etc/timezone /etc/timezone
-chmod 644 /etc/timezone
-chown root:root /etc/timezone
-dpkg-reconfigure --frontend noninteractive tzdata
+if [ ! -e /etc/timezone ]; then
+	cp /vagrant/vendor/sharkodlak/development/filesystem/etc/timezone /etc/timezone
+	chmod 644 /etc/timezone
+	chown root:root /etc/timezone
+	dpkg-reconfigure --frontend noninteractive tzdata
+fi
 
-mkdir --mode=774 /var/log/www
+if [ ! -d /var/log/www ]; then
+	mkdir --mode=774 /var/log/www
+fi
