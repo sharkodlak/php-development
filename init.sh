7928c17a9e669c7ad1c98e21a99290b89d308830
Pavel Stetina <github@shark.kom.cz>
2017-04-13 16:47:08 +0200

    Setup /etc/logrotate.d/php7.1-fpm Setup /etc/php/7.1/fpm/pool.d/www.conf

--- init.sh
+++ init.sh
@@ -8,3 +8,13 @@ fi
 if [ ! -d /var/log/www ]; then
 	mkdir --mode=774 /var/log/www
 fi
+
+HASH=$(sha256sum /etc/logrotate.d/php7.1-fpm | cut -d' ' -f1)
+if [ "31151b05207fe1cc87583ec8a7d2ffafdbbbebe03fe2f36c2b52904341583881" != "$HASH" ]; then
+	cp /vagrant/vendor/sharkodlak/development/filesystem/etc/logrotate.d/php7.1-fpm /etc/logrotate.d/php7.1-fpm
+fi
+
+HASH=$(sha256sum /etc/php/7.1/fpm/pool.d/www.conf | cut -d' ' -f1)
+if [ "22c5ea7ceabbcafc9e6066cb9facb736914e0bcc106748052a0f77c377feb9df" != "$HASH" ]; then
+	patch -b /etc/php/7.1/fpm/pool.d/www.conf < /vagrant/vendor/sharkodlak/development/filesystem/etc/php/7.1/fpm/pool.d/www.conf.patch
+fi
