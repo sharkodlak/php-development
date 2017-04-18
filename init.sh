22486be49cd25e4573ce20ccc6c6e1529a5a88a9
Pavel Stetina <github@shark.kom.cz>
2017-04-18 10:18:23 +0200

    Setup nginx to use PHP-fpm through socket Add vagrant user to adm group

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+usermod -a -G adm vagrant
+
 if [ ! -e /etc/timezone ]; then
 	cp /vagrant/vendor/sharkodlak/development/filesystem/etc/timezone /etc/timezone
 	chmod 644 /etc/timezone
@@ -9,12 +11,20 @@ if [ ! -d /var/log/www ]; then
 	mkdir --mode=774 /var/log/www
 fi
 
+cp -r /vagrant/vendor/sharkodlak/development/filesystem/var/www/* /var/www/
+chown -hR www-data:adm /var/www
+
 HASH=$(sha256sum /etc/logrotate.d/php7.1-fpm | cut -d' ' -f1)
 if [ "31151b05207fe1cc87583ec8a7d2ffafdbbbebe03fe2f36c2b52904341583881" != "$HASH" ]; then
 	cp /vagrant/vendor/sharkodlak/development/filesystem/etc/logrotate.d/php7.1-fpm /etc/logrotate.d/php7.1-fpm
 fi
 
 HASH=$(sha256sum /etc/php/7.1/fpm/pool.d/www.conf | cut -d' ' -f1)
-if [ "22c5ea7ceabbcafc9e6066cb9facb736914e0bcc106748052a0f77c377feb9df" != "$HASH" ]; then
+if [ "e16d2b414516061f6d6433041f5a854c03ba60929af65c0ae12a2b3880f75450" != "$HASH" ]; then
 	patch -b /etc/php/7.1/fpm/pool.d/www.conf < /vagrant/vendor/sharkodlak/development/filesystem/etc/php/7.1/fpm/pool.d/www.conf.patch
+fi
+
+HASH=$(sha256sum /etc/nginx/sites-available/default | cut -d' ' -f1)
+if [ "3b12ca1e6c37e2bdc4081d9bc948159f170b20acbf9996a93ab7abe9748cf8e2" != "$HASH" ]; then
+	cp /vagrant/vendor/sharkodlak/development/filesystem/etc/nginx/sites-available/default /etc/nginx/sites-available/default
 fi
