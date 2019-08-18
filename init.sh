3123f882c6db39c99c6e121e506e718bd59321af
Pavel Stetina <github@shark.kom.cz>
2019-08-18 11:18:53 +0200

    Modify PHP version from 7.1 to 7.3 Modify Postgre version from 9.4 to 11 Force update of Guest Additions

--- init.sh
+++ init.sh
@@ -32,12 +32,18 @@ function updateFile {
 		local source="$(dirname "$1")/$3"
 	fi
 	local hash=$(fileHash $1)
+	local status
 	if [ "$2" == "$hash" ]; then
 		echo skipping already up to date file $1
 		return 1
 	fi
 	echo updating file $1
 	cp "/vagrant/vendor/sharkodlak/development/filesystem$source" "$1"
+	status=$?
+	if [ ! $status ]; then
+		echo -e "\e[0;31mupdate failed"
+	fi
+	return $status
 }
 
 function copyMissingFile {
@@ -50,11 +56,11 @@ function copyMissingFile {
 
 usermod -a -G adm vagrant
 
-if updateFile /etc/locale.gen $(fileHash  /vagrant/vendor/sharkodlak/development/filesystem/etc/locale.gen); then
+if updateFile /etc/locale.gen $(fileHash /vagrant/vendor/sharkodlak/development/filesystem/etc/locale.gen); then
 	locale-gen
 fi
 
-if updateFile /etc/timezone $(fileHash  /vagrant/vendor/sharkodlak/development/filesystem/etc/timezone); then
+if updateFile /etc/timezone $(fileHash /vagrant/vendor/sharkodlak/development/filesystem/etc/timezone); then
 	chmod 644 /etc/timezone
 	chown root:root /etc/timezone
 	dpkg-reconfigure --frontend noninteractive tzdata
@@ -74,28 +80,28 @@ if [ ! -d /var/log/www ]; then
 fi
 
 touch /var/log/www/access.log /var/log/www/error.log /var/log/www/slow.log
-chown -hR www-data:adm /var/www /var/log/www /var/log/php7.1-fpm.log
+chown -hR www-data:adm /var/www /var/log/www /var/log/php7.3-fpm.log
 chmod -R 751 /var/log/www/
-chmod 640 /var/log/php7.1-fpm.log
+chmod 640 /var/log/php7.3-fpm.log
 
-updateFile /etc/logrotate.d/php7.1-fpm 31151b05207fe1cc87583ec8a7d2ffafdbbbebe03fe2f36c2b52904341583881
-patchFile /etc/php/7.1/fpm/pool.d/www.conf 5edb1d606d70d3fb1267507bb8943917a9008cd0bd3013005c9144992761581e
-service php7.1-fpm reload
+updateFile /etc/logrotate.d/php7.3-fpm 31151b05207fe1cc87583ec8a7d2ffafdbbbebe03fe2f36c2b52904341583881
+patchFile /etc/php/7.3/fpm/pool.d/www.conf 5edb1d606d70d3fb1267507bb8943917a9008cd0bd3013005c9144992761581e
+service php7.3-fpm reload
 
 updateFile /etc/nginx/sites-available/default 3b12ca1e6c37e2bdc4081d9bc948159f170b20acbf9996a93ab7abe9748cf8e2
 service nginx reload
 
-apt-get install -y postgresql php7.1-cli php-pgsql php-xdebug
+apt-get install -y postgresql php7.3-cli php-pgsql php-xdebug
 
 copyMissingFile provision/.private/postgres.ini
 parseIniFile provision/.private/postgres.ini
 
 if [[ "$dbname" && "${username[$commonUserIndex]}" ]]; then
 	echo PostgreSQL listen on all interfaces
-	patchFile /etc/postgresql/9.4/main/postgresql.conf 0560ef2b96e5e2ded5e4152f5fa8c3eca78058148cae105165ac0505d254f5a0
+	patchFile /etc/postgresql/11/main/postgresql.conf 27b892ad9084e22e5967ba49881eeba26eb8f5accac22cb754201ba0a0cd226a
 	if [ $? ]; then
 		echo PostgreSQL allow temporal access without password
-		patchFile /etc/postgresql/9.4/main/pg_hba.conf 9b51618284f9c31498b93c6edc355391a05ac44ac846a5c76ca529f0b2b856ec
+		patchFile /etc/postgresql/11/main/pg_hba.conf b888e9bda2f4816171fafb61f0d204d91c4f438f885ef8fd651f908c2f7029a5
 		service postgresql reload
 
 		echo Create database and users
@@ -114,9 +120,10 @@ if [[ "$dbname" && "${username[$commonUserIndex]}" ]]; then
 		chown www-data:adm $dbConnectFile
 		chmod 0640 $dbConnectFile
 
+		echo Revert temporal access without password
+		patchFile /etc/postgresql/11/main/pg_hba.conf anyHash -R
 		echo PostgreSQL allow only password access
-		patchFile /etc/postgresql/9.4/main/pg_hba.conf 5c49a57dd58d76d6c33bdb788cb39ee377d2329df27b7469cea505355ba9d5a3 -R
-		patchFile /etc/postgresql/9.4/main/pg_hba.conf f107b36ec98a5569e33eda69c486da2bdd5d1a07022710bd1276711cdecb6027 0 pg_hba.conf.password.patch
+		patchFile /etc/postgresql/11/main/pg_hba.conf b888e9bda2f4816171fafb61f0d204d91c4f438f885ef8fd651f908c2f7029a5 0 pg_hba.conf.password.patch
 		service postgresql reload
 	fi
 fi
